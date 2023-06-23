666e18cddcd6b3da06640e5ae2a26ec67c201c01
Yann Klein <yann.klein@me.com>
2023-06-23 15:33:30 +0900

    init basic vite+react app with swiper js in it

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './App.css'
+import 'swiper/css';
+
+function App() {
+  return (
+    <>
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+    </Swiper>
+    </>
+  )
+}
+
+export default App
